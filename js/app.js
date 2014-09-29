134ec75f19a31eb011d50c294a15105a98a5df09
David Hugentobler <davidhugentobler30@gmail.com>
2014-09-29 15:24:44 -0600

    completes step 3: Start to Configure the Router

--- js/app.js
+++ js/app.js
@@ -3,5 +3,25 @@ var app = angular.module('nbaRoutes', ['ngRoute']);
 app.config(function($routeProvider, $httpProvider){
   $httpProvider.interceptors.push('httpRequestInterceptor');
 
-  //router here
+$routeProvider
+  .when('/', {
+    templateUrl: 'js/home.homeTmple.html',
+    controller: 'homeCtrl'
+  }).when('/teams/:team', {
+    templateUrl: 'js/teams/teamTmpl.html',
+    controller: 'teamCtrl',
+    // resolves promise from teamServices getTeamData (GET request). The data from that promis is now available in our controller as teamData
+    resolve: {
+      teamData: function (teamService) {
+        return teamService.getTeamData($route.current.params.team);
+      };
+    };
+  }).otherwise('/', {
+    templateUrl: 'js/home.homeTmple.html',
+    controller: 'homeCtrl'
+
+
+
+
+
 });
