9b5a31ef6e090cad3436ed4d16580da92a6d5027
David Hugentobler <davidhugentobler30@gmail.com>
2014-09-30 13:04:23 -0600

    adds comments to new functionality in home folder and app.js

--- js/app.js
+++ js/app.js
@@ -3,17 +3,22 @@ var app = angular.module('nbaRoutes', ['ngRoute']);
 app.config(function($routeProvider, $httpProvider){
   $httpProvider.interceptors.push('httpRequestInterceptor');
 
+// changes template html, controller and data to use based on page user is on('/', '/teams/:team'. etc)
 $routeProvider
   .when('/', {
     templateUrl: 'js/home/homeTmpl.html',
     controller: 'homeCtrl',
+    // resolves promis from homeService getTeamData(GET request). The data from that promis is now in my homeCtrl.js
     resolve: {
+      // data for Los Angeles Lakers
       teamDataLA: function (homeService, $route) {
         return homeService.getTeamData('losangeleslakers');
       },
+      // data for Miami Heat
       teamDataMiami: function (homeService, $route) {
         return homeService.getTeamData('miamiheat');
       },
+      // data for Utah Jazz
       teamDataUtah: function (homeService, $route) {
         return homeService.getTeamData('utahjazz');
       }
@@ -21,9 +26,10 @@ $routeProvider
   }).when('/teams/:team', {
     templateUrl: 'js/teams/teamTmpl.html',
     controller: 'teamCtrl',
-    // resolves promise from teamServices getTeamData (GET request). The data from that promise is now available in our controller as teamData
+    // resolves promise from teamServices getTeamData (GET request). The data from that promise is now available in my teamCtrl.js as teamData
     resolve: {
       teamData: function (teamService, $route) {
+        // $route.current.params.team is the given based on where the user is(the links on the index.html set the url)
         return teamService.getTeamData($route.current.params.team);
       }
     }
--- js/home/homeCtrl.js
+++ js/home/homeCtrl.js
@@ -2,28 +2,24 @@ var app = angular.module('nbaRoutes');
 
 app.controller('homeCtrl', function($scope, homeService, teamDataLA, teamDataMiami, teamDataUtah){
 
-  // makes teamData data now accessible in the view
-  // $scope.teamData = teamData;
-
-  // 
+  // array to ngRepeat and loop through the 3 teams
   $scope.loopNum = [0, 1, 2]
 
-  // 
+  // home object with data about each team as key, value pairs(could have just used 3 different, individual arrays)
   $scope.home = {
     teams: ['losangeleslakers', 'miamiheat', 'utahjazz'],
     homeTeam: ['Los Angeles Lakers', 'Miami Heat', 'Utah Jazz'],
     logoPath: ['images/lakers-logo.png', 'images/heat-logo.png', 'images/jazz-logo.png']
   };
 
-    // 
+    // pushes data from server into teamData array
   $scope.teamData = [];
   $scope.teamData.push(teamDataLA);
   $scope.teamData.push(teamDataMiami);
   $scope.teamData.push(teamDataUtah);
 
 
-  console.log($scope.home);
-  console.log($scope.teamData[0].homeTeamScore);
-  console.log($scope.teamData);
+
+
 
 });
